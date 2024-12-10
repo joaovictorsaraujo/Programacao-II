524f6e7e6836e17ed7ed106da4e2ef40540ed624
20241bsi0319 <joaovictoraraujo.bsi@gmail.com>
2024-12-10 08:04:08 -0300

    Exercício 7

---
+++ exercicios/exercicios_dicionarios2/7_agenda/7_agendatelefonica.py
@@ -0,0 +1,17 @@
+def armazena(contatos):
+    arq = open(contatos, 'rt')
+    linha = arq.readline()
+    agenda = {}
+    
+    while(linha != ''):
+        lst_linha = list(linha.split())
+        if lst_linha[0] not in agenda:
+            agenda[lst_linha[0]] = [lst_linha[1], lst_linha[2]]
+        linha = arq.readline()
+    arq.close()
+    return agenda
+
+def main():
+    arquivo = armazena("7_arquivo.txt")
+    print(armazena(arquivo))
+main()
