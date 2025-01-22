cddc8c64a3b75aa54a0370ad12ca13046cb55723
João Victor S. Araujo <joaovictoraraujo.bsi@gmail.com>
2025-01-22 16:57:31 -0300

    TAD 3

--- exercicios/exercicios_tad/exercicio_03/tad_poli.py
+++ exercicios/exercicios_tad/exercicio_03/tad_poli.py
@@ -11,7 +11,7 @@ def separa_poli(poli):
             termo = ''
             continue
         termo = termo + char
-    lista.append(termo)
+    lista.append(termo.upper())
     if lista[0] == '':
         del lista[0]
     return lista
@@ -32,7 +32,34 @@ def new_poli(poli):
         dic[int(lst[1])] = int(lst[0])
     return dic
 
+def soma_poli(p1, p2):
+    dic = {}
+    for i in p1:
+        dic[i] = p1[i] + p2.get(i,0)
+    return dic
+
+def multiplica_poli(p1, p2):
+    dic = {}
+    for i in p1:
+        for j in p2:
+            dic[i + j] = p1[i] * p2[j]
+    return dic
+
+def subtrai_poli(p1, p2):
+    pS = {}
+    dic = {}
+    for h in p1:
+        pS[h * (-1)] = p1[h] * (-1)
+        
+    for i in pS:
+        dic[i] = pS[i] + p2.get(i, 0)
+    return dic
+
 def main():
-    poli = "-2X4-5X7-3X-X5+44"
+    poli = "-3X2+2X+1"
+    poli2 = "2X2+3X+1"
     print(new_poli(poli))
+    print(new_poli(poli2))
+    print()
+    print(subtrai_poli(new_poli(poli), new_poli(poli2)))
 main()
