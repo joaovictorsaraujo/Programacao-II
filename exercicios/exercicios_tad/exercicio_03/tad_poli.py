8b0882b8198a6472dd04cb77eda1b9bef1681bb6
20241bsi0319 <joaovictoraraujo.bsi@gmail.com>
2025-01-29 13:21:57 -0300

    atualização

---
+++ exercicios/exercicios_tad/exercicio_03/tad_poli.py
@@ -0,0 +1,65 @@
+def separa_poli(poli):
+    termo = ''
+    lista = []
+    for char in poli:
+        if char == '-':
+            lista.append(termo.upper())
+            termo = char
+            continue
+        if char == '+':
+            lista.append(termo.upper())
+            termo = ''
+            continue
+        termo = termo + char
+    lista.append(termo.upper())
+    if lista[0] == '':
+        del lista[0]
+    return lista
+
+def new_poli(poli):
+    lst = separa_poli(poli)
+    dic = {}
+    for n in lst:
+        lst = n.split('X')
+        if len(lst) == 1:
+            lst.append(0)
+        if lst[1] == '':
+            lst[1] = 1 
+        if lst[0] == '':
+            lst[0] = 1    
+        elif lst[0] == '-':
+            lst[0] = '-1'
+        dic[int(lst[1])] = int(lst[0])
+    return dic
+
+def soma_poli(p1, p2):
+    dic = {}
+    for i in p1:
+        dic[i] = p1[i] + p2.get(i,0)
+    return dic
+
+def multiplica_poli(p1, p2):
+    dic = {}
+    for i in p1:
+        for j in p2:
+            dic[i + j] = p1[i] * p2[j]
+    return dic
+
+def subtrai_poli(p1, p2):
+    pS = {}
+    dic = {}
+    for h in p1:
+        pS[h * (-1)] = p1[h] * (-1)
+        
+    for i in pS:
+        dic[i] = pS[i] + p2.get(i, 0)
+    return dic
+
+def main():
+    poli = "-3X2+2X+1"
+    poli2 = "2X2+3X+1"
+    print(new_poli(poli))
+    print(new_poli(poli2))
+    print()
+    print(subtrai_poli(new_poli(poli), new_poli(poli2)))
+main()
